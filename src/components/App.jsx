510993f282b0c5333a3a57aa13f72ea485c1d9f7
Ankur Bajpai <88947414+ankurbajpai125@users.noreply.github.com>
2022-07-24 13:39:43 +0530

    second

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function App() {
+const [Time,setTime] = useState(new Date().toLocaleTimeString("en-US", {
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  hour12: false
+}))
+ 
+ function updateTime(){
+   setTime(new Date().toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false
+  }))
+ }
+ setInterval(updateTime, 1000);
+
+  return (
+    <div className="container">
+      <h1>{Time}</h1>
+      <button onClick = {updateTime}>Get Time</button>
+    </div>
+  );
+}
+
+export default App;
